7a6002b5cfd9ca9f03fde632eaa61895d35eb2eb
Matt White <matthew.white@fclonline.com>
2012-12-04 14:53:36 +0000

    Various Fixes

--- Code/ScriptLibraries/unplugged.js
+++ Code/ScriptLibraries/unplugged.js
@@ -2,14 +2,14 @@ $(window).load( function() {
 
 	$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
 	allowFormsInIscroll();
+
+	initiscroll();
+	$("#menupane").addClass("offScreen");
 	$('.viewsButton').unbind('click');
 	$('.viewsButton').click( function(event) {
 		toggleViewsMenu();
 		return false;
 	});
-
-	$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
-	initiscroll();
 	try {
 		$(".viewlink").each( function() {
 			$(this).addEventListener("click", function() {
@@ -154,13 +154,13 @@ function saveDocument(formid, unid, viewxpagename, formname, parentunid) {
 					 */
 					console.log(response.length);
 					if (response.length == 32) {
-					//	openDocument(
-					//			viewxpagename
-					//					+ "?action=openDocument&documentId="
-					//					+ response, "content");
-					//	initiscroll();
-						$.blockUI();
-						window.location.href = "UnpMain.xsp";
+						openDocument(
+								viewxpagename
+										+ "?action=openDocument&documentId="
+										+ response, "content");
+						initiscroll();
+					//	$.blockUI();
+					//	window.location.href = "UnpMain.xsp";
 					} else {
 						alert(response);
 					}
@@ -252,9 +252,8 @@ function initiscroll() {
 		delete scrollMenu;
 	}catch(e){
 	}
-	$(".iscrollmenu").each( function() {
-		scrollMenu = new iScroll($(this).attr("id"));
-	});
+	scrollMenu = new iScroll('menuPane');
+	
 	$(".iscrollcontent")
 			.each(
 					function() {
@@ -353,7 +352,7 @@ function accordionLoadMore(obj, viewName, catName, xpage){
 
 function fetchDetails(obj, viewName, catName, xpage)
 {	
-	console.log('View: ' + viewName);
+	//console.log('View: ' + viewName);
 	//console.log("Object = " + obj);
 	//Reset category
 	$('.accordionRowSet').empty();
@@ -366,6 +365,7 @@ function fetchDetails(obj, viewName, catName, xpage)
 		$(obj).nextAll('.summaryDataRow:first').children('.loadMoreLink').hide();
 	}
 	else{
+		$('.categoryRow').removeClass("accordianExpanded");
 		accordionLoadMore(obj, viewName, catName, xpage);
 	}
 }
